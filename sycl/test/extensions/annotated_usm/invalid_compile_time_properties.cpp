4b45ba50ae2a4e748094f1c1401ca80b452263c8
Sergey Semenov <sergey.semenov@intel.com>
2025-02-03 10:12:31 -0800

    [WIP] Remove sycl_ext_intel_fpga_kernel_arg_properties

--- sycl/test/extensions/annotated_usm/invalid_compile_time_properties.cpp
+++ sycl/test/extensions/annotated_usm/invalid_compile_time_properties.cpp
@@ -30,100 +30,94 @@ void testInvalidCompileTimeProperty(sycl::queue &q) {
 
   // expected-error@sycl/ext/oneapi/experimental/common_annotated_properties/properties.hpp:* 33+ {{static assertion failed due to requirement 'is_valid_property_for_given_type': Property is invalid for the given type.}}
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}bar_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_device_annotated, N, q, properties{conduit, bar})
+  TEST(malloc_device_annotated, N, q, properties{alignment<8>, bar})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}baz_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_device_annotated, N, dev, Ctx, properties{conduit, buffer_location<1>, baz<5>})
+  TEST(malloc_device_annotated, N, dev, Ctx, properties{alignment<8>, baz<5>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}baz_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_device_annotated<int>, N, q, properties{conduit, baz<2>})
+  TEST(malloc_device_annotated<int>, N, q, properties{alignment<8>, baz<2>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}device_has_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_device_annotated<int>, N, dev, Ctx, properties{buffer_location<1>, device_has<sycl::aspect::cpu>})
+  TEST(malloc_device_annotated<int>, N, dev, Ctx, properties{alignment<8>, device_has<sycl::aspect::cpu>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}work_group_size_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_device_annotated, 1, N, q, properties{register_map, work_group_size<1>})
+  TEST(aligned_alloc_device_annotated, 1, N, q, properties{alignment<8>, work_group_size<1>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}baz_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_device_annotated, 1, N, dev, Ctx, properties{register_map, baz<3>})
+  TEST(aligned_alloc_device_annotated, 1, N, dev, Ctx, properties{alignment<8>, baz<3>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}baz_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_device_annotated<int>, 1, N, q, properties{register_map, baz<0>})
+  TEST(aligned_alloc_device_annotated<int>, 1, N, q, properties{alignment<8>, baz<0>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_device_annotated<int>, 1, N, dev, Ctx, properties{conduit, boo<void>})
+  TEST(aligned_alloc_device_annotated<int>, 1, N, dev, Ctx, properties{alignment<8>, boo<void>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_host_annotated, N, q, properties{conduit, boo<int>})
+  TEST(malloc_host_annotated, N, q, properties{alignment<8>, boo<int>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}streaming_interface_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_host_annotated, N, Ctx, properties{buffer_location<1>, streaming_interface_accept_downstream_stall})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}register_map_interface_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_host_annotated<int>, N, q, properties{register_map_interface_wait_for_done_write, register_map})
+  TEST(malloc_host_annotated, N, Ctx, properties{alignment<8>, streaming_interface_accept_downstream_stall})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_host_annotated<int>, N, Ctx, properties{conduit, buffer_location<1>, boo<int, int>})
+  TEST(malloc_host_annotated<int>, N, Ctx, properties{alignment<8>, boo<int, int>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_host_annotated, 1, N, q, properties{register_map, boo<int, bool>})
+  TEST(aligned_alloc_host_annotated, 1, N, q, properties{alignment<8>, boo<int, bool>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_host_annotated, 1, N, Ctx, properties{register_map, boo<int, int, int>})
+  TEST(aligned_alloc_host_annotated, 1, N, Ctx, properties{alignment<8>, boo<int, int, int>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_host_annotated<int>, 1, N, q, properties{register_map, buffer_location<1>, boo<bool>})
+  TEST(aligned_alloc_host_annotated<int>, 1, N, q, properties{alignment<8>, boo<bool>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_host_annotated<int>, 1, N, Ctx, properties{register_map, buffer_location<1>, boo<float>})
+  TEST(aligned_alloc_host_annotated<int>, 1, N, Ctx, properties{alignment<8>, boo<float>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}device_image_scope_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_shared_annotated, N, q, properties{conduit, device_image_scope})
+  TEST(malloc_shared_annotated, N, q, properties{alignment<8>, device_image_scope})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}host_access_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_shared_annotated, N, dev, Ctx, properties{buffer_location<1>, host_access_read})
+  TEST(malloc_shared_annotated, N, dev, Ctx, properties{alignment<8>, host_access_read})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}boo_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_shared_annotated<int>, N, q, properties{boo<char>, register_map})
+  TEST(malloc_shared_annotated<int>, N, q, properties{boo<char>, alignment<8>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}init_mode_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_shared_annotated<int>, N, dev, Ctx, properties{conduit, buffer_location<1>, init_mode_reset})
+  TEST(malloc_shared_annotated<int>, N, dev, Ctx, properties{alignment<8>, init_mode_reset})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}implement_in_csr_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_shared_annotated, 1, N, q, properties{register_map, implement_in_csr_on})
+  TEST(aligned_alloc_shared_annotated, 1, N, q, properties{alignment<8>, implement_in_csr_on})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}ready_latency_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_shared_annotated, 1, N, dev, Ctx, properties{register_map, ready_latency<1>})
+  TEST(aligned_alloc_shared_annotated, 1, N, dev, Ctx, properties{alignment<8>, ready_latency<1>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}bits_per_symbol_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_shared_annotated<int>, 1, N, q, properties{register_map, bits_per_symbol<1>})
+  TEST(aligned_alloc_shared_annotated<int>, 1, N, q, properties{alignment<8>, bits_per_symbol<1>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}pipelined_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_shared_annotated<int>, 1, N, dev, Ctx, properties{register_map, buffer_location<1>, pipelined<1>})
+  TEST(aligned_alloc_shared_annotated<int>, 1, N, dev, Ctx, properties{alignment<8>, pipelined<1>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}sub_group_size_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_annotated, N, q, alloc::device, properties{conduit, sub_group_size<2>})
+  TEST(malloc_annotated, N, q, alloc::device, properties{alignment<8>, sub_group_size<2>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}uses_valid_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_annotated, N, dev, Ctx, alloc::device, properties{buffer_location<1>, uses_valid_on})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}first_symbol_in_high_order_bits_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_annotated<int>, N, q, alloc::device, properties{first_symbol_in_high_order_bits_on, register_map})
+  TEST(malloc_annotated, N, dev, Ctx, alloc::device, properties{alignment<8>, uses_valid_on})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}protocol_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(malloc_annotated<int>, N, dev, Ctx, alloc::device, properties{conduit, buffer_location<1>, protocol_avalon_streaming_uses_ready})
+  TEST(malloc_annotated<int>, N, dev, Ctx, alloc::device, properties{alignment<8>, protocol_avalon_streaming_uses_ready})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}word_size_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_annotated, 1, N, q, alloc::device, properties{register_map, word_size<1>})
+  TEST(aligned_alloc_annotated, 1, N, q, alloc::device, properties{alignment<8>, word_size<1>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}stride_size_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_annotated, 1, N, dev, Ctx, alloc::device, properties{register_map, stride_size<1>})
+  TEST(aligned_alloc_annotated, 1, N, dev, Ctx, alloc::device, properties{alignment<8>, stride_size<1>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}work_group_size_hint_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_annotated<int>, 1, N, q, alloc::device, properties{register_map, work_group_size_hint<1>})
+  TEST(aligned_alloc_annotated<int>, 1, N, q, alloc::device, properties{alignment<8>, work_group_size_hint<1>})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}latency_anchor_id_key{{.+}}: Found invalid compile-time property in the property list.}}
-  TEST(aligned_alloc_annotated<int>, 1, N, dev, Ctx, alloc::device, properties{register_map, buffer_location<1>, latency_anchor_id<5>})
+  TEST(aligned_alloc_annotated<int>, 1, N, dev, Ctx, alloc::device, properties{alignment<8>, latency_anchor_id<5>})
 }
 
 // clang-format on
--- sycl/test/extensions/annotated_usm/invalid_runtime_properties.cpp
+++ sycl/test/extensions/annotated_usm/invalid_runtime_properties.cpp
@@ -26,106 +26,106 @@ void testInvalidRuntimeProperty(sycl::queue &q) {
   // clang-format off
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_device_annotated, N, q, properties{conduit, cache_config{large_slm}})
+  TEST(malloc_device_annotated, N, q, properties{alignment<8>, cache_config{large_slm}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_device_annotated, N, dev, Ctx, properties{conduit, foo{foo_enum::b}})
+  TEST(malloc_device_annotated, N, dev, Ctx, properties{alignment<8>, foo{foo_enum::b}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_device_annotated<int>), N, q, properties{conduit, foz{0, 1}})
+  TEST((malloc_device_annotated<int>), N, q, properties{alignment<8>, foz{0, 1}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_device_annotated<int>), N, dev, Ctx, properties{conduit, rt_prop1{}})
+  TEST((malloc_device_annotated<int>), N, dev, Ctx, properties{alignment<8>, rt_prop1{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_device_annotated, 1, N, q, properties{conduit, rt_prop2{}})
+  TEST(aligned_alloc_device_annotated, 1, N, q, properties{alignment<8>, rt_prop2{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_device_annotated, 1, N, dev, Ctx, properties{conduit, rt_prop3{}})
+  TEST(aligned_alloc_device_annotated, 1, N, dev, Ctx, properties{alignment<8>, rt_prop3{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_device_annotated<int>), 1, N, q, properties{conduit, rt_prop4{}})
+  TEST((aligned_alloc_device_annotated<int>), 1, N, q, properties{alignment<8>, rt_prop4{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_device_annotated<int>), 1, N, dev, Ctx, properties{conduit, rt_prop5{}})
-
-
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_host_annotated, N, q, properties{conduit, rt_prop6{}})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_host_annotated, N, Ctx, properties{conduit, rt_prop7{}})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_host_annotated<int>), N, q, properties{conduit, rt_prop8{}})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_host_annotated<int>), N, Ctx, properties{conduit, rt_prop9{}})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_host_annotated, 1, N, q, properties{conduit, rt_prop10{}})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_host_annotated, 1, N, Ctx, properties{conduit, rt_prop11{}})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_host_annotated<int>), 1, N, q, properties{conduit, rt_prop12{}})
-
-  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_host_annotated<int>), 1, N, Ctx, properties{conduit, rt_prop13{}})
+  TEST((aligned_alloc_device_annotated<int>), 1, N, dev, Ctx, properties{alignment<8>, rt_prop5{}})
 
 
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_shared_annotated, N, q, properties{conduit, rt_prop14{}})
+  TEST(malloc_host_annotated, N, q, properties{alignment<8>, rt_prop6{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_shared_annotated, N, dev, Ctx, properties{conduit, rt_prop15{}})
+  TEST(malloc_host_annotated, N, Ctx, properties{alignment<8>, rt_prop7{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_shared_annotated<int>), N, q, properties{conduit, rt_prop16{}})
+  TEST((malloc_host_annotated<int>), N, q, properties{alignment<8>, rt_prop8{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_shared_annotated<int>), N, dev, Ctx, properties{conduit, rt_prop17{}})
+  TEST((malloc_host_annotated<int>), N, Ctx, properties{alignment<8>, rt_prop9{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_shared_annotated, 1, N, q, properties{conduit, rt_prop18{}})
+  TEST(aligned_alloc_host_annotated, 1, N, q, properties{alignment<8>, rt_prop10{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_shared_annotated, 1, N, dev, Ctx, properties{conduit, rt_prop19{}})
+  TEST(aligned_alloc_host_annotated, 1, N, Ctx, properties{alignment<8>, rt_prop11{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_shared_annotated<int>), 1, N, q, properties{conduit, rt_prop20{}})
+  TEST((aligned_alloc_host_annotated<int>), 1, N, q, properties{alignment<8>, rt_prop12{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_shared_annotated<int>), 1, N, dev, Ctx, properties{conduit, rt_prop21{}})
+  TEST((aligned_alloc_host_annotated<int>), 1, N, Ctx, properties{alignment<8>, rt_prop13{}})
+
+
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST(malloc_shared_annotated, N, q, properties{alignment<8>, rt_prop14{}})
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST(malloc_shared_annotated, N, dev, Ctx, properties{alignment<8>, rt_prop15{}})
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST((malloc_shared_annotated<int>), N, q, properties{alignment<8>, rt_prop16{}})
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST((malloc_shared_annotated<int>), N, dev, Ctx, properties{alignment<8>, rt_prop17{}})
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST(aligned_alloc_shared_annotated, 1, N, q, properties{alignment<8>, rt_prop18{}})
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST(aligned_alloc_shared_annotated, 1, N, dev, Ctx, properties{alignment<8>, rt_prop19{}})
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST((aligned_alloc_shared_annotated<int>), 1, N, q, properties{alignment<8>, rt_prop20{}})
+
+  // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
+  TEST((aligned_alloc_shared_annotated<int>), 1, N, dev, Ctx, properties{alignment<8>, rt_prop21{}})
   
 
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_annotated, N, q, alloc::device, properties{conduit, rt_prop22{}})
+  TEST(malloc_annotated, N, q, alloc::device, properties{alignment<8>, rt_prop22{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(malloc_annotated, N, q, alloc::device, properties{conduit, rt_prop23{}})
+  TEST(malloc_annotated, N, q, alloc::device, properties{alignment<8>, rt_prop23{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_annotated<int>), N, q, alloc::device, properties{conduit, rt_prop24{}})
+  TEST((malloc_annotated<int>), N, q, alloc::device, properties{alignment<8>, rt_prop24{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((malloc_annotated<int>), N, q, alloc::device, properties{conduit, rt_prop25{}})
+  TEST((malloc_annotated<int>), N, q, alloc::device, properties{alignment<8>, rt_prop25{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_annotated, 1, N, q, alloc::device, properties{conduit, rt_prop26{}})
+  TEST(aligned_alloc_annotated, 1, N, q, alloc::device, properties{alignment<8>, rt_prop26{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST(aligned_alloc_annotated, 1, N, q, alloc::device, properties{conduit, rt_prop27{}})
+  TEST(aligned_alloc_annotated, 1, N, q, alloc::device, properties{alignment<8>, rt_prop27{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_annotated<int>), 1, N, q, alloc::device, properties{conduit, rt_prop28{}})
+  TEST((aligned_alloc_annotated<int>), 1, N, q, alloc::device, properties{alignment<8>, rt_prop28{}})
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
-  TEST((aligned_alloc_annotated<int>), 1, N, q, alloc::device, properties{conduit, rt_prop29{}})
+  TEST((aligned_alloc_annotated<int>), 1, N, q, alloc::device, properties{alignment<8>, rt_prop29{}})
 
 
   // expected-error-re@sycl/ext/oneapi/experimental/annotated_usm/alloc_util.hpp:* {{static assertion failed due to requirement {{.+}}: Found invalid runtime property in the property list.}}
